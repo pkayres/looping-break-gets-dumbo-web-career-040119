0102fc5b68b411af262c7b7133444632f5eae0b0
Pete A <peter.ayresmusic@gmail.com>
2019-03-04 15:30:40 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,8 @@
 
 def levitation_quiz
-  loop do 
+  loop do
     puts "What is the spell that enacts levitation?"
       answer = gets.chomp
       break answer == "Wingardium Leviosa"
       puts "You passed the quiz!"
 end
-
-
